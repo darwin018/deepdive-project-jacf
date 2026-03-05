a17788305382047172fbff0bda9bfc4be7046586
darwin018 <darwin17012005@gmail.com>
2026-03-05 12:02:15 +0530

    changes

--- backend/appsail-python/main.py
+++ backend/appsail-python/main.py
@@ -1,14 +1,7 @@
 from fastapi import FastAPI
-import uvicorn
-import os
-from sqlalchemy import create_engine, text
 
 app = FastAPI()
 
-
-
-@app.get('/')
+@app.get("/")
 def index():
-    return 'Web App with Python FastAPI!'
-
-
+    return {"message": "Web App with Python FastAPI!"}
