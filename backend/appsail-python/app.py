c40c00f2b3b3f82df4b06a2bcdd71367ef82ccee
darwin018 <darwin17012005@gmail.com>
2026-03-05 11:11:57 +0530

    changes

--- backend/appsail-python/app.py
+++ backend/appsail-python/app.py
@@ -5,13 +5,7 @@ from sqlalchemy import create_engine, text
 
 app = FastAPI()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"sslmode": "require"},
-    pool_pre_ping=True
-)
 
 @app.get('/')
 def index():
