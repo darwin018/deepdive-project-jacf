b03c41e933cd5121671af1d087f0135dd449291c
darwin018 <darwin17012005@gmail.com>
2026-03-05 11:27:00 +0530

    changes

--- backend/appsail-python/app.py
+++ backend/appsail-python/app.py
@@ -11,13 +11,4 @@ app = FastAPI()
 def index():
     return 'Web App with Python FastAPI!'
 
-listen_port = int(os.getenv('X_ZOHO_CATALYST_LISTEN_PORT', 8000))
 
-@app.get("/test-db")
-def test_db():
-    with engine.connect() as connection:
-        result = connection.execute(text("SELECT 1"))
-        return {"database": "connected", "result": result.scalar()}
-
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=listen_port)
