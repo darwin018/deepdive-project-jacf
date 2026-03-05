09b9c218c1d0589a74401c2fa72833b80efd9343
darwin018 <darwin17012005@gmail.com>
2026-03-05 10:04:05 +0530

    mar 5 2026

--- backend/appsail-python/app.py
+++ backend/appsail-python/app.py
@@ -4,6 +4,14 @@ import os
 
 app = FastAPI()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"},
+    pool_pre_ping=True
+)
+
 @app.get('/')
 def index():
     return 'Web App with Python FastAPI!'
